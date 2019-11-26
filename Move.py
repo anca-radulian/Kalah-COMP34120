4584ccae0d4de231ae14da07f5a5204a688703a4
Vlad Vasile <vlad.vasile@student.manchester.ac.uk>
2019-11-26 17:40:22 +0000

    Pythonified versions for the Kalah and Move classes

---
+++ Move.py
@@ -0,0 +1,31 @@
+import Side;
+
+# Represents a move (not a turn) in the Kalah game.
+class Move:
+
+  # The side of the board the player making the move is playing on.
+  Side _side;
+
+  """
+   The hole from which seeds are picked at the beginning of the move and
+   distributed. It has to be >= 1.
+  int _hole;
+  """
+
+  """
+   side = The side of the board the player making the move is playing on.
+   hole = The hole from which seeds are picked at the beginning of
+   the move and distributed. It has to be >= 1.
+  """
+  def _init_(self, side, hole):
+    if hole < 1:
+      raise ValueError('Hole numbers must be >= 1, but ' + hole + ' was given.');
+
+    self.side = side;
+    self.hole = hole;
+
+  def getSide(self):
+    return self.side;
+
+  def getHole(self):
+    return self.hole;
