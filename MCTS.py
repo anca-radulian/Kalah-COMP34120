e8cd03a5c8d108eeaedec242c8679ae73ad339e6
unknown <gheorghiutuodr@gmail.com>
2019-12-05 19:49:33 +0000

    Started MCTS class

--- MCTS.py
+++ MCTS.py
@@ -1,6 +1,10 @@
 import math
 import random
-
+from Kalah import Kalah
 class MCTS:
     def __init__(self,kalahBoard, side):
-        self.root =
+        self.kalahBoard = kalahBoard
+        self.side = side
+
+    def expansion(self, kalahBoard):
+        board = kalahBoard.getBoard()
